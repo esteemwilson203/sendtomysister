b486c5512d2c43e7eb311ebedf3405583949dfd5
esteemwilson203 <semester4fpt@gmail.com>
2024-05-31 11:08:22 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Thư của Lan😊😊😊",
+    introTitle: 'Gửi chị Mai yêu dấu của em (Lan)😊😊😘',
+    introDesc: `Em quý chị lắm lắm nì 🤭🤭😌`,
+    btnIntro: 'Chị cũng quý em lắm!!!🤭🤭',
+
+
+    title: 'Đã quý e rồi thì phải nấu cơm, rửa bát, mua đồ ăn vặt cho em nì😂😂😂',
+    desc: '🥰🥰🥰',
+    btnYes: 'Ok chốt mọi thứ để chị lo <33',
+    btnNo: 'Không làm đâu :3',
+    question: 'Wow chị của em là nhất, em quý chị nhất hihi<3',
+    btnReply: 'Gửi em',
+    reply: 'Yên tâm nha chi làm được! ahihi🥰',
+    mess: 'WOW 🥰. Em lại thích quá 😘😘',
+    messDesc: 'E quý chị nhiều ',
+    btnAccept: 'Chốt lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100024631578372'
+}
